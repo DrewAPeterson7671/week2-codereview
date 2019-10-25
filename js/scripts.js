7fc13b125c0673b712f35cbddc65c31a442ba03f
Student <example@example.com>
2019-10-25 10:57:51 -0700

    Address a problem in select form, but still has a lingering issue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   $("#formInput").submit(function(event) {
     var nameOne = $("input#nameOne").val();
-
+    var favPlatform = $("input#favPlatform").val();
 
     $("#nameOneOut").text(nameOne);
+    $("#favPlatform").text(favPlatform);
 
     $(".surveyResults").hide();
     $(".surveyParrot").hide(); //considering joining with surveyResults
