43a7fb541b3e2b18aa092003240a62c68e5fc747
Student <example@example.com>
2019-10-25 14:24:17 -0700

    flesh out the branching logic and landing place of the html output.

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,39 @@ $(document).ready(function() {
     var favOs = $("input:radio[name=favOs]:checked").val();
     var favLocation = $("input:radio[name=favLocation]:checked").val();
     var personalDisposition = $("input:radio[name=personalDisposition]:checked").val();
+    var count = 0;
+
+
 
     if (favPlatform === 'Mobile' && favOs === 'Apple') {
-      console.log("Swift");
-    } else {
-      consol.log("bad logic");
-    }
+      $("#languageSwift").show();
+      count += 1;
+    } else if {
+      favPlatform === 'Mobile' && favOs === "Google" {
+        $("#languageGo").show();
+        count += 1;
+      } else if {
+        favorite === 'Systems & Servers' && favOs === "Windows" {
+          $("#languageCs").show();
+        }
+      }
+    } 
+
+
+
+
+
+
+
 
     $("#nameOneOut").text(nameOne);
     $("#favPlatformOut").text(favPlatform);
     $("#favOsOut").text(favOs);
     $("#favLocationOut").text(favLocation);
     $("#personalDispositionOut").text(personalDisposition);
-
+    $("#language1Out").text(recLanguage1);
+    $("#language2Out").text(recLanguage2);
+    $("#language3Out").text(recLanguage3);
 
 //    console.log(favPlatform);
 
